ad70cc49e89e55009329e1a2b859665907c67b6d
Carlos Amengual <carlosame644+github@gmail.com>
2025-12-13 20:18:10 +0100

    Update SPDX headers #31

--- src/module-info.java
+++ src/module-info.java
@@ -1,12 +1,14 @@
 /*
  *
- * Copyright (c) 2005-2024, Carlos Amengual.
- *
- * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2005-2025, Carlos Amengual.
  *
  * Licensed under a BSD-style License. You can find the license here:
  * https://css4j.github.io/LICENSE.txt
  *
+ */
+/*
+  SPDX-License-Identifier: BSD-3-Clause OR BSD-2-Clause OR LGPL-2.1-or-later OR
+   GPL-2.0-or-later
  */
 
 /**
